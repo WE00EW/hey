b324fc4eeb1e9cf78445dfbc7ef2e0f79388c426
周兴邦 <v_xbzzhou@tencent.com>
2019-11-27 15:32:25 +0800

    start

---
+++ applet/miniprogram/pages/start/start.js
@@ -0,0 +1,3 @@
+"use strict";
+var app = getApp();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhcnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJzdGFydC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQ0EsSUFBTSxHQUFHLEdBQUcsTUFBTSxFQUFjLENBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJcbmNvbnN0IGFwcCA9IGdldEFwcDxJQXBwT3B0aW9uPigpIl19
